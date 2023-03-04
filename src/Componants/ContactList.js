3f182476e99f303b38aad29244ba96fb2b16f1e2
Akshay Patil <83501069+Akshay-1003@users.noreply.github.com>
2023-03-04 23:49:37 +0530

    taskis done

---
+++ src/Componants/ContactList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function ContactList(props) {  
+   let css={
+    marginLeft:'10em',
+    fontSize: '18px',
+    border: '1px solid red'
+   }
+  return (
+    <>
+    <div className='container'>
+      <div className='pic' style={css}>
+        <img src='https://api.lorem.space/image/movie?w=150&h=220'></img> 
+        
+      </div>
+        <h4  style={css}>akshay Patil</h4>
+    </div>
+    
+    </>
+    
+  )
+}
